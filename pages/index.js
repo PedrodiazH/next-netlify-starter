f80599dfdf6dfb73397ef28df80c91f72e40201a
PedrodiazH <133780341+PedrodiazH@users.noreply.github.com>
2023-05-16 09:49:41 -0400

    index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Wena compare" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Prueba para ver si funciona esta shiet
         </p>
       </main>
 
